0d33a15c926aec6005c2c3e57932dab539a3f97a
Sydney Shugarman <srs238@miami.edu>
2023-04-09 20:50:34 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = async (phase, { defaultConfig }) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    /* config options here */
+  }
+  return nextConfig
 }
-
-module.exports = nextConfig
