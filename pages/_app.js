e025a029d8455fbd9cabedfc9192b3c9f91c90c2
Sydney Shugarman <srs238@miami.edu>
2023-04-09 20:28:34 -0400

    adding

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
-import '@/styles/globals.css'
+import Preloader from "@/src/components/Preloader";
+import Switcher from "@/src/components/Switcher";
+import SalimovHead from "@/src/SalimovHead";
+import "@/styles/globals.css";
+import { Fragment } from "react";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }) => {
+  return (
+    <Fragment>
+      <SalimovHead />
+      <Switcher />
+      <Preloader />
+      <Component {...pageProps} />
+    </Fragment>
+  );
+};
+export default App;
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,22 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import Header from "@/src/components/Header";
-import ScrollBar from "@/src/components/ScrollBar";
-import About from "@/src/components/sections/About";
+import styles from '../styles/Home.module.css'
+import Header from "../src/components/Header";
+import ScrollBar from "../src/components/ScrollBar";
+import About from "../src/components/sections/About";
 // import Blog from "@/src/components/sections/Blog";
 // import Clients from "@/src/components/sections/Clients";
 // import Contact from "@/src/components/sections/Contact";
-import Copyright from "@/src/components/sections/Copyright";
+import Copyright from "../src/components/sections/Copyright";
 // import Facts from "@/src/components/sections/Facts";
-import Home from "@/src/components/sections/Home";
-import Portfolio from "@/src/components/sections/Portfolio";
-import Testimonials from "@/src/components/sections/Testimonials";
-import Separator from "@/src/components/Separator";
-import { jqueryFuntion } from "@/src/utilits";
-import { Fragment, useEffect } from "react";
-import styles from '@/styles/Home.module.css'
+import Home from "../src/components/sections/Home";
+import Portfolio from "../src/components/sections/Portfolio";
+import Testimonials from "../src/components/sections/Testimonials";
+import Separator from "../src/components/Separator";
+import { jqueryFuntion } from "../src/utilits";
 
-const inter = Inter({ subsets: ['latin'] })
+import { Fragment, useEffect } from "react";
+
 const Index = () => {
   useEffect(() => {
     jqueryFuntion();
